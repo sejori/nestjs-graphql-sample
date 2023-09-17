d6c20561219d70bc58d92d9ea425756171eae579
Sebastien Ringrose <seb@codeworks.me>
2023-09-17 17:02:05 +0100

    test: extend seed data

--- test/mock.data.ts
+++ test/mock.data.ts
@@ -70,5 +70,77 @@ export const seedUsers = [
     email: 'diego.rodriguez@example.com',
     createdAt: new Date('2023-09-12T09:30:00Z'),
     updatedAt: new Date('2023-09-12T10:00:00Z')
+  },
+  {
+    id: '7d4f4f12-87cd-4c62-bc18-9e8b64d3f72f',
+    firstName: 'Sophia',
+    lastName: 'Johnson',
+    email: 'sophia.johnson@example.com',
+    createdAt: new Date('2023-09-11T14:30:00Z'),
+    updatedAt: new Date('2023-09-11T15:00:00Z')
+  },
+  {
+    id: '82e1c2e5-663d-48ed-afdd-012b2c171b10',
+    firstName: 'Liam',
+    lastName: 'Martinez',
+    email: 'liam.martinez@example.com',
+    createdAt: new Date('2023-09-10T11:45:00Z'),
+    updatedAt: new Date('2023-09-10T12:15:00Z')
+  },
+  {
+    id: '9a8e0a8d-2c9b-45c7-b24c-3b3b1fe09b5f',
+    firstName: 'Olivia',
+    lastName: 'Taylor',
+    email: 'olivia.taylor@example.com',
+    createdAt: new Date('2023-09-09T17:20:00Z'),
+    updatedAt: new Date('2023-09-09T17:50:00Z')
+  },
+  {
+    id: 'bca891f9-47db-4bfe-8825-67a5e8698563',
+    firstName: 'Mason',
+    lastName: 'Garcia',
+    email: 'mason.garcia@example.com',
+    createdAt: new Date('2023-09-08T10:10:00Z'),
+    updatedAt: new Date('2023-09-08T10:40:00Z')
+  },
+  {
+    id: '6479f394-6e6b-4939-97d3-8edf92f005a8',
+    firstName: 'Ava',
+    lastName: 'Miller',
+    email: 'ava.miller@example.com',
+    createdAt: new Date('2023-09-07T16:55:00Z'),
+    updatedAt: new Date('2023-09-07T17:25:00Z')
+  },
+  {
+    id: 'ea915a91-6b3e-4d0a-8f25-7d8ca65d24d2',
+    firstName: 'Noah',
+    lastName: 'Thomas',
+    email: 'noah.thomas@example.com',
+    createdAt: new Date('2023-09-06T12:20:00Z'),
+    updatedAt: new Date('2023-09-06T12:50:00Z')
+  },
+  {
+    id: 'c51db999-f0a9-415c-b0e2-6b2a57d71c1b',
+    firstName: 'Isabella',
+    lastName: 'Brown',
+    email: 'isabella.brown@example.com',
+    createdAt: new Date('2023-09-05T09:45:00Z'),
+    updatedAt: new Date('2023-09-05T10:15:00Z')
+  },
+  {
+    id: 'f7e7ec18-ec41-4af6-9fb7-7e2b8b9a8a19',
+    firstName: 'Liam',
+    lastName: 'Clark',
+    email: 'liam.clark@example.com',
+    createdAt: new Date('2023-09-04T14:30:00Z'),
+    updatedAt: new Date('2023-09-04T15:00:00Z')
+  },
+  {
+    id: 'd3e8e502-5a95-421b-8a5d-5818e01ff7a2',
+    firstName: 'Mia',
+    lastName: 'Wilson',
+    email: 'mia.wilson@example.com',
+    createdAt: new Date('2023-09-03T18:45:00Z'),
+    updatedAt: new Date('2023-09-03T19:15:00Z')
   }
 ];
